8204944e4fc647e48a593f5a3978033fc6db5eed
HelixU <shenzheng_hz@foxmail.com>
2016-09-23 01:00:39 +0800

    Initial Commit

---
+++ MyAlertView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyAlertView
+//
+//  Created by ShenZheng on 16/9/23.
+//  Copyright © 2016年 ShenZheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyAlertView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyAlertView
+//
+//  Created by ShenZheng on 16/9/23.
+//  Copyright © 2016年 ShenZheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
